0ab8f95b57851833864a524d131bbc0b52e4fcbf
Luke Passmore <43517238+lukepassmore@users.noreply.github.com>
2019-12-26 22:16:25 +0000

    Initial commit

---
+++ MyShop/Services/OfflineDataStore.cs
@@ -0,0 +1,43 @@
+﻿using MyShop.Services;
+using Newtonsoft.Json;
+using Plugin.EmbeddedResource;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using Xamarin.Essentials;
+
+[assembly: Dependency(typeof(OfflineDataStore))]
+namespace MyShop.Services
+{
+    public class OfflineDataStore : IDataStore
+    {
+
+        public async Task<IEnumerable<Store>> GetStoresAsync()
+        {
+            var json = ResourceLoader.GetEmbeddedResourceString(Assembly.Load(new AssemblyName("MyShop")), "stores.json");
+            return await Task.Run(() => JsonConvert.DeserializeObject<List<Store>>(json));
+        }
+
+        public async Task AddFeedbackAsync(Feedback feedback) =>
+            await Email.ComposeAsync("My Shop Feedback", feedback.ToString(), "james.montemagno@xamarin.com");
+
+        public Task<Store> AddStoreAsync(Store store) => Task.FromResult(store);
+
+        public async Task<IEnumerable<Feedback>> GetFeedbackAsync() =>
+             await Task.Run(() => { return new List<Feedback>(); });
+        
+        public Task Init() => Task.Run(() => { });
+        
+        public Task<bool> RemoveFeedbackAsync(Feedback feedback) => Task.FromResult(true);
+
+        public Task<bool> RemoveStoreAsync(Store store) => Task.FromResult(true);
+
+        public Task SyncFeedbacksAsync() =>  Task.Run(() => { });
+        
+        public Task SyncStoresAsync() => Task.Run(() => { });
+        
+        public Task<Store> UpdateStoreAsync(Store store) => Task.FromResult(store);
+    }
+}
---
+++ MyShop/ViewModels/StoreViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Xamarin.Forms;
+using Plugin.ExternalMaps;
+using MyShop.Helpers;
+
+using Xamarin.Essentials;
+
+namespace MyShop
+{
+    public class StoreViewModel : ViewModelBase
+    {
+        public Store Store { get; set; }
+        public string Monday { get { return string.Format("{0} - {1}", Store.MondayOpen, Store.MondayClose); } }
+        public string Tuesday { get { return string.Format("{0} - {1}", Store.TuesdayOpen, Store.TuesdayClose); } }
+        public string Wednesday { get { return string.Format("{0} - {1}", Store.WednesdayOpen, Store.WednesdayClose); } }
+        public string Thursday { get { return string.Format("{0} - {1}", Store.ThursdayOpen, Store.ThursdayClose); } }
+        public string Friday { get { return string.Format("{0} - {1}", Store.FridayOpen, Store.FridayClose); } }
+        public string Saturday { get { return string.Format("{0} - {1}", Store.SaturdayOpen, Store.SaturdayClose); } }
+        public string Sunday { get { return string.Format("{0} - {1}", Store.SundayOpen, Store.SundayClose); } }
+
+
+        public string Address1 { get { return Store.StreetAddress; } }
+        public string Address2 { get { return string.Format("{0}, {1} {2}", Store.City, Store.State, Store.ZipCode); } }
+
+        public StoreViewModel(Store store, Page page) : base(page)
+        {
+            this.Store = store;
+        }
+
+        Command navigateCommand;
+        public Command NavigateCommand
+        {
+            get
+            {
+                return navigateCommand ?? (navigateCommand = new Command(() =>
+                CrossExternalMaps.Current.NavigateTo(Store.Name, Store.Latitude, Store.Longitude)));
+            }
+        }
+
+        Command callCommand;
+        public Command CallCommand
+        {
+            get
+            {
+                return callCommand ?? 
+                    (callCommand = new Command(() =>
+                        PhoneDialer.Open(Store.PhoneNumber.CleanPhone())
+                    ));
+            }
+        }
+
+    }
+}
+
